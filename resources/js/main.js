1c2a73cb266f50707926cf4969550b6e14bc671e
dcorn068 <daniel@raventelemetry.com>
2020-03-24 10:56:56 -0400

    install Prettier VS Code extension and enable setting 'format on save'

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,23 +1,18 @@
-const table = document.getElementById('table')
-const tableBody = document.querySelector('tbody')
-const graphArea = document.getElementById('graph')
-
+const table = document.getElementById("table");
+const tableBody = document.querySelector("tbody");
+const graphArea = document.getElementById("graph");
 
 const names = [];
 
 const getData = () => {
-
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then( response => response.json() )
-    .then( users => {
-      
-      users.map( user => names.push(user.name) );
-      const analyzedNames = countCharacters( names );
-      insertTableRow( tableBody, analyzedNames );
-      addBarGraph( 'Characters', 'Names', analyzedNames, graphArea );
-      
-    } )
-
-}
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => {
+      users.map(user => names.push(user.name));
+      const analyzedNames = countCharacters(names);
+      insertTableRow(tableBody, analyzedNames);
+      addBarGraph("Characters", "Names", analyzedNames, graphArea);
+    });
+};
 
 getData();
